a7c92ecfac3beb090db1ee77d9f687422136a59f
lololowka225 <125420798+lololowka225@users.noreply.github.com>
2023-03-01 14:56:09 +1300

    Add files via upload

---
+++ 01_sandpit.py
@@ -0,0 +1,10 @@
+like_coffee = input("do you like coffee? ").lower()
+
+if like_coffee == "no":
+    print("Wrong answer, coffee is great")
+
+elif like_coffee == "yes":
+    print("me too")
+
+else:
+    print("??  Answer yes / no")
